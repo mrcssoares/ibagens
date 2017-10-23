b9b2b3048ec15db85552b8569fd8065bb89c058e
Marcos Soares Souza <mss3@icomp.ufam.edu.br>
2017-10-23 00:45:27 -0400

    Adicionado features de feat, crop, resize sem alterar o aspecto de altura em relação a largura. Outros ajustes

--- app/controller/images.js
+++ app/controller/images.js
@@ -6,8 +6,6 @@ const media = require('../models/media');
 let execProcess = require('../utils/execprocess');
 let fs   = require('fs');
 
-Consts = require('../../config/const');
-let consts = new Consts();
 
 
 
@@ -123,29 +121,71 @@ module.exports = function () {
 
 };
 
-//espera um vetor de comandos
+let flags1 = {
+    //redimenciona sem considerar ao aspectos da imagem
+    rdf: '\\!',
+    //redimenciona considerando os aspectos da imagem, não a deixa ser maior que seu tamanho
+    rdi: '\\>',
+    //redimenciona cortando as areas delimitadas
+    crop: '^',
+};
+
+let flags = [];
+flags['fill'] = '^';
+flags['crop'] = '^ \ -gravity center -extent :wx:h';
+flags['gray'] = ' -colorspace Gray -gamma 2.2';
+flags[''] = '\\!';
+
+/**
+ * commands: vetor de comandos
+ * @return {string}
+ */
 function IdentifyCommands(commands) {
     console.log(commands);
     //[ 'w_150', 'r_150', 'batata', 'arroz', 'mulheres' ]
     if(commands.length === 1) {
         if (commands[0].split('_')[0] === 'w') {
-          return resize(commands[0].split('_')[1], null, consts.IM.rdf);
+            return resize(commands[0].split('_')[1], null, flags['']);
         }
         if(commands[0].split('_')[0] === 'h'){
-          return resize(null, commands[0].split('_')[1], consts.IM.rdf);
+            return resize(commands[0].split('_')[1], null, flags['']);
         }
-    }
-    if(commands.length === 2){
-        if (commands[0].split('_')[0] === 'w' && commands[1].split('_')[0] === 'h') {
-            return resize(commands[0].split('_')[1], commands[1].split('_')[1], consts.IM.rdf);
+        if(commands[0].split('_')[0] === 'p'){
+            return commands[0].split('_')[1]*100 + '%';
         }
-        if (commands[0].split('_')[0] === 'h' && commands[1].split('_')[0] === 'w') {
-            return resize(commands[0].split('_')[1], commands[1].split('_')[1], consts.IM.rdf);
-        }
-    }
-    if(commands.length > 2){
+        if(commands[0] === 'gray');
+            return flags[commands[0]];
 
     }
+
+    //se dois parametros
+    if(commands.length === 2){
+        if (commands[0].split('_')[0] === 'w' && commands[1].split('_')[0] === 'h') {
+            return resize(commands[0].split('_')[1], commands[1].split('_')[1], flags['']);
+        }
+        if (commands[0].split('_')[0] === 'h' && commands[1].split('_')[0] === 'w') {
+            return resize(commands[1].split('_')[1], commands[0].split('_')[1], flags['']);
+        }
+
+        //provisorio: caso um valor + o gray_scale
+        if (commands[0].split('_')[0] === 'w' && commands[1] === 'gray') {
+            return resize(commands[0].split('_')[1], null, flags['']) + flags[commands[1]];
+        }
+        if (commands[0].split('_')[0] === 'h' && commands[1] === 'gray') {
+            return resize(commands[0].split('_')[1], null, flags['']) + flags[commands[1]];
+        }
+    }
+
+    //se mais de dois parametros
+    if(commands.length > 2){
+        if (commands[0].split('_')[0] === 'w' && commands[1].split('_')[0] === 'h') {
+            return resize(commands[0].split('_')[1], commands[1].split('_')[1], flags[String(commands[2])].replace(':w',commands[0].split('_')[1]).replace(':h',commands[1].split('_')[1])); // + flags[commands[3]];
+        }
+        if (commands[0].split('_')[0] === 'h' && commands[1].split('_')[0] === 'w') {
+            return resize(commands[1].split('_')[1], commands[0].split('_')[1], flags[String(commands[2])].replace(':w',commands[1].split('_')[1]).replace(':h',commands[0].split('_')[1])); // + flags[commands[3]];
+        }
+    }
+
     return null;
 }
 
@@ -159,8 +199,6 @@ function resize(w, h, i) {
         return w + 'x' + h + i;
     else if(w)
         return w + 'x' + w + i;
-    else if(h)
-        return h + 'x' + h + i;
 }
 
 
--- config/const.js
+++ config/const.js
@@ -10,13 +10,6 @@ method.secretKey = "minhaChaveSuperSecreta";
 
 //comandos do IM
 
-method.IM = {
-    //redimenciona sem considerar ao aspectos da imagem
-    rdf: '\\!',
-    //redimenciona considerando os aspectos da imagem, não a deixa ser maior que seu tamanho
-    rdi: '\\>',
-    //redimenciona cortando as areas delimitadas
-    crop: '^',
-};
+
 
 module.exports = Constants;
